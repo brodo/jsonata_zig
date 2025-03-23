67a5519562103c3572537211ce948a06d097ded4
Julian Dax <julian.dax@posteo.de>
2025-03-23 23:12:28 +0100

    Refactor `get_datasets` to return parsed JSON dataset map.
    Revised `get_datasets` to return a `StringArrayHashMap` of parsed JSON values instead of void. Added JSON parsing and error handling for dataset files, along with proper resource deallocation in the test case.

--- tools/generate_tests.zig
+++ tools/generate_tests.zig
@@ -28,21 +28,20 @@ pub fn main() !void {
 }
 
 const formatter = std.fs.path.fmtJoin(&[_][]const u8{ "test-suite", "datasets" });
-fn get_datasets(alloc: std.mem.Allocator) void {
+fn get_datasets(alloc: std.mem.Allocator) std.StringArrayHashMap(std.json.Parsed(std.json.Value)) {
     var dataset_path = std.ArrayList(u8).init(alloc);
     defer dataset_path.deinit();
     dataset_path.writer().print("{s}", .{formatter}) catch |err| {
         fatal("unable to create path: {s}", .{@errorName(err)});
     };
-
     var test_suite_dir = std.fs.cwd().openDir(dataset_path.items, .{}) catch |err| {
         fatal("unable to open '{s}': {s}", .{ dataset_path.items, @errorName(err) });
     };
     defer test_suite_dir.close();
     var iter = test_suite_dir.iterate();
+    var string_hash_map = std.StringArrayHashMap(std.json.Parsed(std.json.Value)).init(alloc);
     while (iter.next()) |maybe_entry| {
         const entry = maybe_entry orelse break;
-        std.debug.print("File {s} ({})\n", .{ entry.name, entry.kind });
         if (entry.kind == .file) {
             var file = test_suite_dir.openFile(entry.name, .{}) catch |err| {
                 fatal("unable to open '{s}': {s}", .{ dataset_path.items, @errorName(err) });
@@ -52,11 +51,17 @@ fn get_datasets(alloc: std.mem.Allocator) void {
                 fatal("unable to read '{s}': {s}", .{ entry.name, @errorName(err) });
             };
             defer alloc.free(file_content);
-            std.debug.print("File content: \n{s}", .{file_content});
+            const parsed = std.json.parseFromSlice(std.json.Value, alloc, file_content, .{}) catch |err| {
+                fatal("unable to parse '{s}': {s}", .{ entry.name, @errorName(err) });
+            };
+            string_hash_map.put(entry.name, parsed) catch |err| {
+                fatal("unable to alloc: {any}", .{err});
+            };
         }
     } else |err| {
         fatal("error iterating: {s}", .{@errorName(err)});
     }
+    return string_hash_map;
 }
 
 fn fatal(comptime format: []const u8, args: anytype) noreturn {
@@ -65,5 +70,11 @@ fn fatal(comptime format: []const u8, args: anytype) noreturn {
 }
 
 test "get_datasets should return datasets" {
-    get_datasets(std.testing.allocator);
+    var datasets = get_datasets(std.testing.allocator);
+    defer datasets.deinit();
+    var iter = datasets.iterator();
+    defer while (iter.next()) |entry| {
+        entry.value_ptr.deinit();
+    };
+    try std.testing.expectEqual(28, datasets.count());
 }
