ed29aeacfb26a39761bc97de634a8fd7720a5e92
Julian Dax <julian.dax@posteo.de>
2025-03-23 17:35:13 +0100

    initial commit

---
+++ tools/generate_tests.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_state.deinit();
+    const arena = arena_state.allocator();
+
+    const args = try std.process.argsAlloc(arena);
+
+    if (args.len != 2) fatal("wrong number of arguments", .{});
+
+    const output_file_path = args[1];
+
+    var output_file = std.fs.cwd().createFile(output_file_path, .{}) catch |err| {
+        fatal("unable to open '{s}': {s}", .{ output_file_path, @errorName(err) });
+    };
+    defer output_file.close();
+
+    try output_file.writeAll(
+        \\const std = @import("std");
+        \\const testing = std.testing;
+        \\
+        \\test "failing to add" {
+        \\  try testing.expect(3 + 8 == 11);
+        \\}
+    );
+    return std.process.cleanExit();
+}
+
+fn fatal(comptime format: []const u8, args: anytype) noreturn {
+    std.debug.print(format, args);
+    std.process.exit(1);
+}
